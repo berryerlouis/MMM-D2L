59f017dafc6187a80dfbcd09aa69ea4abb58f147
Louis Berryer <louisberryer@gmail.com>
2022-12-12 21:59:41 +0100

    fix module error

--- d2l.js
+++ d2l.js
@@ -1,5 +1,3 @@
-
-
 const D2LApi = {
     GET_DATA_REQ: 'GET_DATA_REQ',
     GET_DATA_RES: 'GET_DATA_RES',
@@ -9,28 +7,24 @@ const D2LApi = {
     COMPTEUR_URL: 'https://d2lapi.sicame.io/api/D2L/D2Ls',
     COMPTEUR_REQ: 'COMPTEUR_REQ',
     COMPTEUR_RES: 'COMPTEUR_RES',
-    LAST_INDEX_URL: function(compteurId)
-    {
+    LAST_INDEX_URL: function (compteurId) {
         return `https://d2lapi.sicame.io/api/D2L/D2Ls/${compteurId}/LastIndexes`
     },
     LAST_INDEX_REQ: 'LAST_INDEX_REQ',
     LAST_INDEX_RES: 'LAST_INDEX_RES',
-    LAST_INDEXES_URL: function(compteurId,nbHoursToFetch)
-    {
+    LAST_INDEXES_URL: function (compteurId, nbHoursToFetch) {
         let current = new Date().toISOString();
         let yesterday = new Date(new Date().getTime() - (parseInt(nbHoursToFetch) * 60 * 60 * 1000)).toISOString();
         return `https://d2lapi.sicame.io/api/D2L/D2Ls/${compteurId}/IndexesBetween?from=${yesterday}&to=${current}`
     },
     LAST_INDEXES_REQ: 'LAST_INDEXES_REQ',
     LAST_INDEXES_RES: 'LAST_INDEXES_RES',
-    LAST_CURRENT_URL: function(compteurId)
-    {
+    LAST_CURRENT_URL: function (compteurId) {
         return `https://d2lapi.sicame.io/api/D2L/D2Ls/${compteurId}/LastCurrents`
     },
     LAST_CURRENT_REQ: 'LAST_CURRENT_REQ',
     LAST_CURRENT_RES: 'LAST_CURRENT_RES',
-    LAST_CURRENTS_URL: function(compteurId,nbHoursToFetch)
-    {
+    LAST_CURRENTS_URL: function (compteurId, nbHoursToFetch) {
         let current = new Date().toISOString();
         let yesterday = new Date(new Date().getTime() - (parseInt(nbHoursToFetch) * 60 * 60 * 1000)).toISOString();
         return `https://d2lapi.sicame.io/api/D2L/D2Ls/${compteurId}/CurrentsBetween?from=${yesterday}&to=${current}`
@@ -38,4 +32,4 @@ const D2LApi = {
     LAST_CURRENTS_REQ: 'LAST_CURRENTS_REQ',
     LAST_CURRENTS_RES: 'LAST_CURRENTS_RES',
 };
-module.exports = { D2LApi }
+module?.exports = { D2LApi }
